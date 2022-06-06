0455425e4929dc13ff1261101184be4604870ec1
deboracarv <deborab.carvalho@hotmail.com>
2022-06-06 15:23:59 -0300

    Anotacoes em desenvolvimento web atualizado

---
+++ Desenvolvimento Web/projetos/pagina.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<title>Meu primeiro site em PHP! Whohoo!</title>
+	</head>
+	
+	<body>
+		<?php
+			for ($i = 0 ; $i < 10 ; $i++) {
+				print("Linha número " . $i . "<br />");
+			}
+		?>
+	</body>
+	
+</html>
